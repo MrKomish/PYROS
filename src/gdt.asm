fc5893198d0e0b22ac1258aa1734f65640106b5c
Michael <misha.konviser@gmail.com>
2018-12-29 20:34:22 +0200

    Added LICENSE

--- src/gdt.asm
+++ src/gdt.asm
@@ -15,6 +15,7 @@ gdt_flush:
 	gdt_flush_code:
 		jmp 0x08: gdt_flush_data
 
+    ; The following 7 lines of code were taken from Micro-OS (Amit Saroussi is the real king)
 	gdt_flush_data:
 		mov ax, 0x10 ;; data segment
 

db8add96905079df5b5de9cc8677f13ff0e7d78b
Michael <misha.konviser@gmail.com>
2019-01-22 21:35:52 +0200

    Refactored paging and fixed it

--- backup/paging.cc
+++ backup/paging.cc
@@ -56,3 +56,64 @@ public:
 };
 
 // add-symbol-file ~/Desktop/pyros-clean/cmake-build-debug/pyros.elf 0xc0000000
+
+/*uint32_t k_base_physical_page_addr = (((uint32_t) kernel_base_addr) / PAGE_SIZE) * PAGE_SIZE;
+uint32_t k_end_physical_page_addr = (((uint32_t) kernel_end_addr) / PAGE_SIZE) * PAGE_SIZE;
+
+k_end_physical_page_addr += PAGE_SIZE;
+
+k_end_physical_page_addr[0] =
+
+
+uint32_t k_page_tables_amount = k_base_physical_page_i - k_end_physical_page_i + 1;
+uint32_t k_page_directories_amount = k_page_tables_amount / PAGE_DIRECTORY_TABLES_AMOUNT;
+
+page_table_t* page_entries_physical_page = (page_table_t *) (kernel_end_physical_page + 0x1000);
+
+uint16_t dir_i = 0;
+uint16_t table_i = 0;
+
+for (uint32_t p_addr = kernel_base_physical_page; p_addr <= kernel_end_physical_page; p_addr += 0x1000) {
+
+    page_table_t* page_entry = &page_entries_physical_page[dir_i * 0x400 + table_i];
+
+    *page_entry = PAGE_TABLE_PRESENT(1) | PAGE_TABLE_WRITABLE(1) | PAGE_TABLE_USER_ACCESSIBLE(0)
+            | PAGE_TABLE_WRITE_THROUGH(0) | PAGE_TABLE_CACHE_DISABLED(0) | PAGE_TABLE_ACCESSED(0) | PAGE_TABLE_DIRTY(0) | PAGE_TABLE_GLOBAL(0)
+            | p_addr;
+
+    table_i++;
+
+    if (table_i == PAGE_DIRECTORY_TABLES_AMOUNT) {
+        page_dir_t *page_dir = &page_dirs[dir_i];
+        *page_dir = PAGE_DIRECTORY_PRESENT(1) | PAGE_DIRECTORY_WRITABLE(1) | PAGE_DIRECTORY_USER_ACCESSIBLE(0)
+                    | PAGE_DIRECTORY_WRITE_THROUGH(0) | PAGE_DIRECTORY_CACHE_DISABLED(0) |
+                    PAGE_DIRECTORY_ACCESSED(0) | PAGE_DIRECTORY_PAGE_SIZE(0) | PAGE_DIRECTORY_TABLE_ADDRESS(0)
+                    | p_addr;
+
+        table_i = 0;
+        dir_i++;
+    }
+
+}
+
+for (; table_i < PAGE_DIRECTORY_TABLES_AMOUNT; table_i++) {
+    // p_addr += 0x1000;
+}
+
+for (int i = 0; i < PAGE_DIRECTORIES_AMOUNT; i++) {
+
+}*/
+
+/*typedef struct {
+    uint8_t present : 1;
+    uint8_t writable : 1;
+    uint8_t user_accessible : 1;
+    uint8_t write_through : 1;
+    uint8_t cache_disabled : 1;
+    uint8_t accessed : 1;
+    uint8_t zero : 1;
+    uint8_t page_size : 1;
+    uint8_t ignored : 1;
+    uint8_t available : 3;
+    uint32_t tables_address : 20;
+} page_dir_t;*/
--- src/memory/paging.h
+++ src/memory/paging.h
@@ -38,22 +38,6 @@
 typedef uint32_t page_dir_t;
 typedef uint32_t page_table_t;
 
-/*typedef struct {
-    uint8_t present : 1;
-    uint8_t writable : 1;
-    uint8_t user_accessible : 1;
-    uint8_t write_through : 1;
-    uint8_t cache_disabled : 1;
-    uint8_t accessed : 1;
-    uint8_t zero : 1;
-    uint8_t page_size : 1;
-    uint8_t ignored : 1;
-    uint8_t available : 3;
-    uint32_t tables_address : 20;
-} page_dir_t;*/
-
-// page_dir_t page_dirs[1024] __attribute__((aligned(4096)));
-
 uint32_t k_base_physical_page_addr;
 uint32_t k_end_physical_page_addr;
 
@@ -64,86 +48,58 @@ uint32_t k_end_physical_page_addr;
 page_dir_t kernel_page_dirs[PAGE_DIRECTORIES_AMOUNT]  __attribute__((aligned(4096)));
 page_table_t kernel_page_tables[PAGE_DIRECTORIES_AMOUNT][PAGE_DIRECTORY_TABLES_AMOUNT]  __attribute__((aligned(4096)));
 
-void init_kernel_paging() {
-    k_base_physical_page_addr = (((uint32_t) kernel_base_addr) / PAGE_SIZE) * PAGE_SIZE;
-    k_end_physical_page_addr = (((uint32_t) kernel_end_addr) / PAGE_SIZE) * PAGE_SIZE;
+void map_tables(uint16_t dir_i, uint16_t table_start_i, uint16_t table_end_i, uint32_t base_phys_page_addr,
+                uint32_t table_flags) {
+    for (uint16_t page_i = table_start_i; page_i < table_end_i; page_i++) {
 
-    uint16_t kernel_directory_start_i = 0xc0000000 >> 22;
-    uint16_t kernel_directory_end_i = 0xffffffff >> 22;
+        page_table_t *table = &kernel_page_tables[dir_i][page_i];
 
-    for (uint16_t dir_i = kernel_directory_start_i; dir_i < kernel_directory_end_i; dir_i++) {
-        page_dir_t* dir = &kernel_page_dirs[dir_i];
-
-        *dir = PAGE_DIRECTORY_PRESENT(1) | PAGE_DIRECTORY_WRITABLE(1) | PAGE_DIRECTORY_USER_ACCESSIBLE(0)
-                    | PAGE_DIRECTORY_WRITE_THROUGH(0) | PAGE_DIRECTORY_CACHE_DISABLED(0) |
-                    PAGE_DIRECTORY_ACCESSED(0) | PAGE_DIRECTORY_PAGE_SIZE(0)
-                    | ((uint32_t) &kernel_page_tables[dir_i]);
-
-        for (uint16_t page_i = 0; page_i < PAGE_DIRECTORY_TABLES_AMOUNT; page_i++) {
-            page_table_t* table = &kernel_page_tables[dir_i][page_i];
-
-            *table = PAGE_TABLE_PRESENT(1) | PAGE_TABLE_WRITABLE(1) | PAGE_TABLE_USER_ACCESSIBLE(0)
-                    | PAGE_TABLE_WRITE_THROUGH(0) | PAGE_TABLE_CACHE_DISABLED(0) | PAGE_TABLE_ACCESSED(0) | PAGE_TABLE_DIRTY(0) | PAGE_TABLE_GLOBAL(0)
-                    | PAGE_TABLE_PHYSICAL_ADDRESS((dir_i - kernel_directory_start_i) * PAGE_DIRECTORY_TABLES_AMOUNT + page_i);
-        }
+        *table = table_flags | ((uint32_t) base_phys_page_addr + (page_i - table_start_i) * PAGE_SIZE);
     }
 }
 
+void map_directories(uint16_t dir_start_i, uint16_t dir_end_i, uint16_t base_phys_dir_i, uint32_t dir_flags,
+                     uint32_t table_flags) {
+    for (uint16_t dir_i = dir_start_i; dir_i < dir_end_i; dir_i++) {
+        page_dir_t *dir = &kernel_page_dirs[dir_i];
+
+        *dir = dir_flags | ((uint32_t) &kernel_page_tables[dir_i]);
+
+        map_tables(dir_i, 0, PAGE_DIRECTORY_TABLES_AMOUNT,
+                   ((uint32_t) base_phys_dir_i + (dir_i - dir_start_i)) * PAGE_DIRECTORY_TABLES_AMOUNT * PAGE_SIZE,
+                   table_flags);
+    }
+}
+
+/**
+ * Map 0x00000000 - 0x3fffffff -> 0x00000000 - 0x3fffffff
+ */
+void init_kernel_paging() {
+    uint16_t kernel_directory_start_i = 0x00000000 >> 22;
+    uint16_t kernel_directory_end_i = 0x3fffffff >> 22;
+
+    map_directories(
+            kernel_directory_start_i, kernel_directory_end_i, 0,
+            PAGE_DIRECTORY_PRESENT(1) | PAGE_DIRECTORY_WRITABLE(1),
+            PAGE_TABLE_PRESENT(1) | PAGE_TABLE_WRITABLE(1)
+    );
+
+}
+
+
 void enable_paging() {
+    init_kernel_paging();
+
     set_page_directory((const uint32_t) kernel_page_dirs);
     set_cr0(get_cr0() | CR0_PAGING_ENABLE);
 }
 
 void kinit_paging() {
+    k_base_physical_page_addr = (((uint32_t) kernel_base_addr) / PAGE_SIZE) * PAGE_SIZE;
+    k_end_physical_page_addr = (((uint32_t) kernel_end_addr) / PAGE_SIZE) * PAGE_SIZE;
+
     init_kernel_paging();
     enable_paging();
-
-    /*uint32_t k_base_physical_page_addr = (((uint32_t) kernel_base_addr) / PAGE_SIZE) * PAGE_SIZE;
-    uint32_t k_end_physical_page_addr = (((uint32_t) kernel_end_addr) / PAGE_SIZE) * PAGE_SIZE;
-
-    k_end_physical_page_addr += PAGE_SIZE;
-
-    k_end_physical_page_addr[0] =
-
-
-    uint32_t k_page_tables_amount = k_base_physical_page_i - k_end_physical_page_i + 1;
-    uint32_t k_page_directories_amount = k_page_tables_amount / PAGE_DIRECTORY_TABLES_AMOUNT;
-
-    page_table_t* page_entries_physical_page = (page_table_t *) (kernel_end_physical_page + 0x1000);
-
-    uint16_t dir_i = 0;
-    uint16_t table_i = 0;
-
-    for (uint32_t p_addr = kernel_base_physical_page; p_addr <= kernel_end_physical_page; p_addr += 0x1000) {
-
-        page_table_t* page_entry = &page_entries_physical_page[dir_i * 0x400 + table_i];
-
-        *page_entry = PAGE_TABLE_PRESENT(1) | PAGE_TABLE_WRITABLE(1) | PAGE_TABLE_USER_ACCESSIBLE(0)
-                | PAGE_TABLE_WRITE_THROUGH(0) | PAGE_TABLE_CACHE_DISABLED(0) | PAGE_TABLE_ACCESSED(0) | PAGE_TABLE_DIRTY(0) | PAGE_TABLE_GLOBAL(0)
-                | p_addr;
-
-        table_i++;
-
-        if (table_i == PAGE_DIRECTORY_TABLES_AMOUNT) {
-            page_dir_t *page_dir = &page_dirs[dir_i];
-            *page_dir = PAGE_DIRECTORY_PRESENT(1) | PAGE_DIRECTORY_WRITABLE(1) | PAGE_DIRECTORY_USER_ACCESSIBLE(0)
-                        | PAGE_DIRECTORY_WRITE_THROUGH(0) | PAGE_DIRECTORY_CACHE_DISABLED(0) |
-                        PAGE_DIRECTORY_ACCESSED(0) | PAGE_DIRECTORY_PAGE_SIZE(0) | PAGE_DIRECTORY_TABLE_ADDRESS(0)
-                        | p_addr;
-
-            table_i = 0;
-            dir_i++;
-        }
-
-    }
-
-    for (; table_i < PAGE_DIRECTORY_TABLES_AMOUNT; table_i++) {
-        // p_addr += 0x1000;
-    }
-
-    for (int i = 0; i < PAGE_DIRECTORIES_AMOUNT; i++) {
-
-    }*/
 }
 
 #endif //PAGING_H

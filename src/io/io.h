bb8735d52356931aa72c1ef663526a9ccdadbf00
Michael Konviser <misha.konviser@gmail.com>
2019-09-06 13:52:17 +0300

    .

--- src/io/io.h
+++ src/io/io.h
@@ -10,7 +10,7 @@ typedef uint16_t port_t;
  * Wait by writing to unused port 0x80
  */
 static inline void io_wait() {
-    __asm__ __volatile__ ( "outb %%al, $0x80" : : "a" (0) );
+    asm volatile ( "outb %%al, $0x80" : : "a" (0) );
 }
 
 /**
@@ -19,7 +19,7 @@ static inline void io_wait() {
  * @param val
  */
 static inline void outb(port_t port, uint8_t val) {
-    __asm__ __volatile__ ( "outb %%al, %%dx" : : "d" (port), "a" (val));
+    asm volatile ( "outb %%al, %%dx" : : "d" (port), "a" (val));
 }
 
 /**
@@ -28,7 +28,7 @@ static inline void outb(port_t port, uint8_t val) {
  * @param val
  */
 static inline void outw(port_t port, uint16_t val) {
-    __asm__ __volatile__ ( INTEL_START "outw %%dx, %%ax" INTEL_END : : "d" (port), "a" (val));
+    asm volatile ( INTEL_START "outw %%dx, %%ax" INTEL_END : : "d" (port), "a" (val));
 }
 
 /**
@@ -37,7 +37,7 @@ static inline void outw(port_t port, uint16_t val) {
  * @param val
  */
 static inline void outl(port_t port, uint32_t val) {
-    __asm__ __volatile__ ( INTEL_START "outl %%dx, %%eax" INTEL_END : : "d" (port), "a" (val));
+    asm volatile ( INTEL_START "outl %%dx, %%eax" INTEL_END : : "d" (port), "a" (val));
 }
 
 /**
@@ -47,7 +47,7 @@ static inline void outl(port_t port, uint32_t val) {
  */
 static inline uint8_t inb(port_t port) {
     uint8_t ret;
-    __asm__ __volatile__ ( "inb %%dx, %%al" : "=a" (ret): "d" (port));
+    asm volatile ( "inb %%dx, %%al" : "=a" (ret): "d" (port));
     return ret;
 }
 
@@ -58,7 +58,7 @@ static inline uint8_t inb(port_t port) {
  */
 static inline uint16_t inw(port_t port) {
     uint16_t ret;
-    __asm__ __volatile__ ( INTEL_START "inb %%ax, %%dx" INTEL_END : "=a" (ret): "d" (port));
+    asm volatile ( INTEL_START "inb %%ax, %%dx" INTEL_END : "=a" (ret): "d" (port));
     return ret;
 }
 
@@ -69,98 +69,18 @@ static inline uint16_t inw(port_t port) {
  */
 static inline uint32_t inl(port_t port) {
     uint32_t ret;
-    __asm__ __volatile__ ( INTEL_START "inl %%eax, %%dx" INTEL_END : "=a" (ret) : "d" (port));
+    asm volatile ( INTEL_START "inl %%eax, %%dx" INTEL_END : "=a" (ret) : "d" (port));
     return ret;
 }
 
 static inline void enable_interrupts ()
 {
-    __asm__ __volatile__ ( INTEL_START "sti" INTEL_END );
+    asm volatile ( INTEL_START "sti" INTEL_END );
 }
 
 static inline void disable_interrupts ()
 {
-    __asm__ __volatile__ ( INTEL_START "cli" INTEL_END );
+    asm volatile ( INTEL_START "cli" INTEL_END );
 }
-
-//
-///**
-// * Wait by writing to unused port 0x80
-// */
-//static inline void io_wait() {
-//    asm volatile ( "out 0x80, %%al" : : "a"(0) );
-//}
-//
-///**
-// * Write byte to port
-// * @param port
-// * @param val
-// */
-//static inline void outb(port_t port, uint8_t val) {
-//    asm volatile (
-//            "outb %0, %1;" :: "a"(val), "Nd"(port));
-//}
-//
-///**
-// * Write word to port
-// * @param port
-// * @param val
-// */
-//static inline void outw(port_t port, uint16_t val) {
-//    asm volatile ("outb %0, %1;" :: "a"(val), "Nd"(port));
-//    asm volatile ("outw dx, ax" :: "d" (port), "a" (val));
-//}
-//
-///**
-// * Write long to port
-// * @param port
-// * @param val
-// */
-//static inline void outl(port_t port, uint32_t val) {
-//    asm volatile ("outl dx, eax" : : "d" (port), "a" (val));
-//}
-//
-///**
-// * Read byte from port
-// * @param port
-// * @param val
-// */
-//static inline uint8_t inb(port_t port) {
-//    uint8_t ret;
-//    asm volatile ("inb %0, %w1": "=a" (ret): "Nd" (port));
-//    return ret;
-//}
-//
-///**
-// * Read word from port
-// * @param port
-// * @param val
-// */
-//static inline uint16_t inw(port_t port) {
-//    uint16_t ret;
-//    asm volatile ("inb %0, %w1": "=a" (ret): "Nd" (port));
-//    return ret;
-//}
-//
-///**
-// * Read long from port
-// * @param port
-// * @param val
-// */
-//static inline uint32_t inl(port_t port) {
-//    uint32_t ret;
-//    asm volatile ("inl %0, %w1" : "=a" (ret) : "Nd" (port));
-//    return ret;
-//}
-//
-//static inline void enable_interrupts ()
-//{
-//    asm volatile ("sti");
-//}
-//
-//static inline void disable_interrupts ()
-//{
-//    asm volatile ("cli");
-//}
 
 #endif //PYROS_IOPORT_H
